05e7ed97ae9780c0554549d4d32927a1d969775f
LXavier133 <lxddantas@gmail.com>
2024-09-11 10:49:13 -0300

    Cabeçalho comum aos Labs

---
+++ modelo.cpp
@@ -0,0 +1,9 @@
+/* Leonardo Xavier Dantas		*/
+/* Turma 28.4				*/
+/* Exercício				*/
+/*					*/
+/* Programa Compilado com G++ 9.4.0	*/
+// Instituto Tecnológico de Aeronáutica
+// CES-11: Algoritimos e Estruturas de Dados
+// Prof. Armando
+// data
